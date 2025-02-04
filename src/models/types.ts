bc99d2e223be3c5c716fc6f7f02c64f6a117815a
2noScript <thangmk7@gmail.com>
2025-02-05 01:34:16 +0700

    feat: rule crawl product info

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,6 +1,7 @@
 type TYPE_LANGUAGE = "en" | "vi";
 type TYPE_lOCALE = "en_EN" | "vi_VN";
 type Status = "SUCCESS" | "ERROR" | "WITCH_OUT";
+type Unit="VND"
 
 export enum Platforms {
   Lazada = "lazada",
@@ -18,6 +19,17 @@ export interface IResponseListProduct {
 
 export interface IProductInfo {
   name: string;
+  price:number,
+  discountPrice?:number
+  originPrice?:number
+  unit:Unit,
+  star?:number,
+  discount?:number,
+  imageUrlThumbnail:string,
+  imagePathThumbnail?:string,
+  location?:string
+  favourite?:boolean,
+  description?:string
 }
 
 export interface ISourceInfo {
